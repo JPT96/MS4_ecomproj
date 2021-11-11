09a6fcab2a5d1145cf3e2503272d6d2fe77a92c2
JPT96 <joshuathomas96@msn.com>
2021-11-11 21:32:00 +0000

    create a cart func that call the cart html

--- shop/views.py
+++ shop/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def shop(request):
     context = {}
     return render (request, 'shop/shop.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'shop/cart.html', context)
