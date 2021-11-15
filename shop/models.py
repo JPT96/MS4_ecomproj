12882e62d3cd009d0c08b6eaa4b02d685fae31fa
JPT96 <joshuathomas96@msn.com>
2021-11-15 19:18:53 +0000

    create a product class in models

--- shop/models.py
+++ shop/models.py
@@ -3,9 +3,11 @@ from django.contrib.auth.models import user
 # Create your models here.
 
 class Shopper(models.model):
-    user= models.OneToOneField(User, null=True, blank=True, on_delete = models.CASCADE)
+    user= models.OneToOneField(User, on_delete=models.CASCADE null=True, blank=True, on_delete = models.CASCADE)
     name= models.CharField(max_length=200, null=True)
     email=model.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
+
+class Product()
