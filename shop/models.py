6c02648beb8e16877b09ee51f63683caa007a2cf
JPT96 <joshuathomas96@msn.com>
2021-11-15 17:18:35 +0000

    def str to return self.name

--- shop/models.py
+++ shop/models.py
@@ -6,3 +6,6 @@ class Shopper(models.model):
     user= models.OneToOneField(User, null=True, blank=True, on_delete = models.CASCADE)
     name= models.CharField(max_length=200, null=True)
     email=model.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
