7da09581fa90bc58227292a470bb88d31e6262ae
JPT96 <joshuathomas96@msn.com>
2021-11-15 19:25:34 +0000

    return the string value again

--- shop/models.py
+++ shop/models.py
@@ -15,3 +15,6 @@ class Product(models.model):
     price = models.FloatField()
     digital = models.BooleanField(default=False,null=True, blank=True)
 
+    def __str__(self):
+        return self.name
+
