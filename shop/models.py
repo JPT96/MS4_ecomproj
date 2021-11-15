ad8475fc3312ef58ca4cb0713ad26f881d055948
JPT96 <joshuathomas96@msn.com>
2021-11-15 17:16:12 +0000

    add models of max length 200 email and name

--- shop/models.py
+++ shop/models.py
@@ -3,3 +3,6 @@ from django.contrib.auth.models import user
 # Create your models here.
 
 class Shopper(models.model):
+    user= models.OneToOneField(User, null=True, blank=True, on_delete = models.CASCADE)
+    name= models.CharField(max_length=200, null=True)
+    email=model.CharField(max_length=200, null=True)
