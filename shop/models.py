6fb41d5d06d862a5f7c6863ea67a91452c4529fa
JPT96 <joshuathomas96@msn.com>
2021-11-15 22:30:07 +0000

    return self address in models.py

--- shop/models.py
+++ shop/models.py
@@ -41,3 +41,6 @@ class ShippingAddress(models.model):
     county = models.CharField(max_length=200,null=False)
     postcode = models.CharField(max_length=200,null=False)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
