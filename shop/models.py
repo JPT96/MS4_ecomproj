5265fe76fdf0beefa91306056612dcdc0361dd4a
JPT96 <joshuathomas96@msn.com>
2021-11-16 15:23:34 +0000

    Correct spelling mistakes and debugging

--- shop/models.py
+++ shop/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import user
+from django.contrib.auth.models import User
 # Create your models here.
 
-class Shopper(models.model):
-    user= models.OneToOneField(User, on_delete=models.CASCADE null=True, blank=True, on_delete = models.CASCADE)
+class Shopper(models.Model):
+    user= models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True,)
     name= models.CharField(max_length=200, null=True)
-    email=model.CharField(max_length=200, null=True)
+    email=models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
 
-class Product(models.model):
+class Product(models.Model):
     name =models.CharField(max_length=200)
     price = models.FloatField()
     digital = models.BooleanField(default=False,null=True, blank=True)
@@ -18,8 +18,8 @@ class Product(models.model):
     def __str__(self):
         return self.name
 
-class Order(models.model):
-    customer = models.ForeginKey(Customer, on_delete=models.set_NULL, null=True, blank=True)
+class Order(models.Model):
+    customer = models.ForeignKey(Shopper, on_delete=models.SET_NULL, null=True, blank=True)
     date_order = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100, null=True)
@@ -27,15 +27,15 @@ class Order(models.model):
     def __str__(self):
         return str(self.id)
 
-class OrderItem(models.model):
-    product = models.ForeginKey(Product, on_delete=models.set_NULL, null=True) 
-    order = models.ForeginKey(Order, on_delete=models.set_NULL, null=True) 
-    quantity= models.IntergerField(default=0, null=True, blank=True)
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True) 
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True) 
+    quantity= models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
-class ShippingAddress(models.model):
-    customer = models.ForeginKey(Shopper, on_delete=models.set_NULL, null=True)
-    order = models.ForeginKey(Order, on_delete=models.set_NULL, null=True)
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Shopper, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
     address = models.CharField(max_length=200,null=False)
     city = models.CharField(max_length=200,null=False)
     county = models.CharField(max_length=200,null=False)
