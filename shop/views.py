76d8bbf19231b764f8e7560f007e3474e8fb46e8
JPT96 <joshuathomas96@msn.com>
2021-11-18 19:21:14 +0000

    create a empty list for else for now

--- shop/views.py
+++ shop/views.py
@@ -12,6 +12,8 @@ def cart(request):
         shopper = request.user.shopper
         order, created = Order.objects.get_or_create(shopper=shopper, complete=False)
         item = order.orderitem_set.all()
+    else:
+        items = []
     context = {}
     return render(request, 'shop/cart.html', context)
 
