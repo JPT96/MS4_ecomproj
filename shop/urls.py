56ba5330ad7c8b8cecaff39b146eb9881f53753d
JPT96 <joshuathomas96@msn.com>
2021-11-19 15:37:44 +0000

    change name and path to update_item

--- shop/urls.py
+++ shop/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.shop, name="shop"),
     path('checkout/', views.checkout, name='checkout'),
     path('cart/', views.cart, name="cart"),
-    path('cart/', views.updateitem, name="cart")
+    path('update_item/', views.updateitem, name="update_item")
 ]
