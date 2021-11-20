ac05705d56d9a56c9d13f2ca8640d799364e72f6
JPT96 <joshuathomas96@msn.com>
2021-11-20 12:45:20 +0000

    correct views.py indentation for shop

--- shop/views.py
+++ shop/views.py
@@ -5,12 +5,14 @@ from .models import *
 # Create your views here.
 def shop(request):
 
-     if request.user.is_authenticated:
+
+    if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
     else:
+
         items = []
         order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
         cartItems = ['get_cart_items']
