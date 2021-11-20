580478b676458b8f966cb8ae8f4d0f447bd6b8b7
JPT96 <joshuathomas96@msn.com>
2021-11-20 19:59:18 +0000

    add comma before the path

--- shop/urls.py
+++ shop/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.shop, name="shop"),
     path('checkout/', views.checkout, name='checkout'),
     path('cart/', views.cart, name="cart"),
-    path('update_item/', views.updateitem, name="update_item")
+    path('update_item/', views.updateitem, name="update_item"),
     path('process_order/', views.processOrder, name='process_order')
 ]
