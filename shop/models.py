8956b2cb2f84e1af8b41ac0bfbf261abfcfd44ca
JPT96 <joshuathomas96@msn.com>
2021-11-20 19:08:35 +0000

    name var shippingInfo for the data

--- shop/models.py
+++ shop/models.py
@@ -71,6 +71,7 @@ class ShippingAddress(models.Model):
     city = models.CharField(max_length=200,null=False)
     county = models.CharField(max_length=200,null=False)
     postcode = models.CharField(max_length=200,null=False)
+    country = models.CharField(max_length=200,null=False)
     date_added = models.DateTimeField(auto_now_add=True)
 
     @property
