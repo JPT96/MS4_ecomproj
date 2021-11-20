d3dbb65e185e8031ecbf22ec342e546e1df2fb29
JPT96 <joshuathomas96@msn.com>
2021-11-20 12:16:28 +0000

    views.py add shipping:false in else for checkout

--- shop/views.py
+++ shop/views.py
@@ -42,7 +42,7 @@ def checkout(request):
         
     else:
         items = []
-        order = {'get_cart_total':0, 'get_cart_items':0}
+        order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
         cartItems = ['get_cart_items']
     context = {'items':items, 'order':order, 'cartItems':cartItems}
      
